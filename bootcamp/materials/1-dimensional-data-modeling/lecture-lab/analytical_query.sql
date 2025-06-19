84aaa237d46bfaa3f0d5fc5dbc3562d67e030e39
Cường <tranvinhcuong@gmail.com>
2025-06-19 16:59:46 +0900

    Avoid error without casting, just need parentheses around.
    To avoid Postgres error `[42P01] ERROR: missing FROM-clause entry for table`, need to wrap the column from CTE with parentheses. Remove castings still work, so remove the them.

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/analytical_query.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/analytical_query.sql
@@ -1,7 +1,7 @@
  SELECT player_name,
-        (seasons[cardinality(seasons)]::season_stats).pts/
-         CASE WHEN (seasons[1]::season_stats).pts = 0 THEN 1
-             ELSE  (seasons[1]::season_stats).pts END
+        (seasons[cardinality(seasons)]).pts/
+         CASE WHEN (seasons[1]).pts = 0 THEN 1
+             ELSE  (seasons[1]).pts END
             AS ratio_most_recent_to_first
  FROM players
  WHERE current_season = 1998;
